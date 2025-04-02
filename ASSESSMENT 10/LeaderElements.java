41b0f5dc26222ef1d3293c91fc863f99b7667afa
SaiBalaji-6834 <saibalajimangapuram@gmail.com>
2025-04-02 16:04:06 +0530

    Create LeaderElements.java

---
+++ ASSESSMENT 10/LeaderElements.java
@@ -0,0 +1,17 @@
+public class LeaderElements {
+    public static void printLeaders(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            boolean isLeader = true;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] < arr[j]) {
+                    isLeader = false;
+                    break;
+                }
+            }
+            if (isLeader) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
