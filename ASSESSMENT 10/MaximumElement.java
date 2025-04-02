1321f9df1fc61b51d29da919423bd6980c0fd475
SaiBalaji-6834 <saibalajimangapuram@gmail.com>
2025-04-02 16:02:56 +0530

    Create MaximumElement.java

---
+++ ASSESSMENT 10/MaximumElement.java
@@ -0,0 +1,11 @@
+public class MaximumElement {
+    public static int findMax(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+}
