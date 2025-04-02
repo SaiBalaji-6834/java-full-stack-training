c28d25837e826ed978e763894448c0eae22cf635
SaiBalaji-6834 <saibalajimangapuram@gmail.com>
2025-04-02 16:08:20 +0530

    Create MajorityElement.java

---
+++ ASSESSMENT 10/MajorityElement.java
@@ -0,0 +1,12 @@
+public class MajorityElement {
+    public static int majorityElement(int[] arr) {
+        int count = 0, candidate = -1;
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
