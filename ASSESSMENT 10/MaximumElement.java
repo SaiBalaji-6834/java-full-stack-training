ac5aa6514654b269475abb793da3fcfe760627da
SaiBalaji-6834 <saibalajimangapuram@gmail.com>
2025-04-02 16:14:42 +0530

    Update MaximumElement.java

--- ASSESSMENT 10/MaximumElement.java
+++ ASSESSMENT 10/MaximumElement.java
@@ -8,4 +8,9 @@ public class MaximumElement {
         }
         return max;
     }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Maximum element: " + findMax(arr));
+    }
 }
