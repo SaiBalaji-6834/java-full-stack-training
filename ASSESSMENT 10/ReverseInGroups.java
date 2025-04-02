638cd5ee40a5a43bea2aaf5e546e57b0b9ea9097
SaiBalaji-6834 <saibalajimangapuram@gmail.com>
2025-04-02 16:15:43 +0530

    Update ReverseInGroups.java

--- ASSESSMENT 10/ReverseInGroups.java
+++ ASSESSMENT 10/ReverseInGroups.java
@@ -12,4 +12,11 @@ public class ReverseInGroups {
             }
         }
     }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int k = 3;
+        reverseInGroups(arr, k);
+        System.out.println("Array after reverse in groups: " + java.util.Arrays.toString(arr));
+    }
 }
