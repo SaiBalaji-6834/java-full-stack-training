c16442bb064730924a149ad091ea4261c9e5a4d5
SaiBalaji-6834 <saibalajimangapuram@gmail.com>
2025-04-02 16:07:04 +0530

    Create BinarySort.java

---
+++ ASSESSMENT 10/BinarySort.java
@@ -0,0 +1,17 @@
+public class BinarySort {
+    public static void binarySort(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                count++;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (i < count) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+        }
+    }
+}
